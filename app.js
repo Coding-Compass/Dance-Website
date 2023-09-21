1f7d6c30dc11f5610436b55cbc5a3e2d15c016da
Sumit <sumitsharma72324@gmail.com>
2023-09-21 20:10:57 +0530

    Made Changes Again!

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 const express = require('express');
+const app = express();
+const path = require("path");
+const port = 80;
 app.use('/static', express.static('static')) // For serving static files
 app.use(express.urlencoded());
 
 // PUG SPECIFIC STUFF
 app.set('view engine', 'pug') // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) // Set the views directory
- 
+
 // ENDPOINTS
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     const params = {};
     res.status(200).render('index.pug', params);
 })
-app.get('/Contact-Us', (req, res)=>{
+app.get('/Contact-Us', (req, res) => {
     const params = {};
     res.status(200).render('contact.pug', params);
 })
-app.get('/Services', (req, res)=>{
+app.get('/Services', (req, res) => {
     const params = {};
     res.status(200).render('services.pug', params);
 })
-app.get('/Classes', (req, res)=>{
+app.get('/Classes', (req, res) => {
     const params = {};
     res.status(200).render('classes.pug', params);
 })
-app.get('/About-us', (req, res)=>{
+app.get('/About-us', (req, res) => {
     const params = {};
     res.status(200).render('about.pug', params);
 })
-app.get('/sponser', (req, res)=>{
+app.get('/sponser', (req, res) => {
     const params = {};
     res.status(200).render('sponser.pug', params);
 })
 
 
-    // START THE SERVER
-app.listen(port, ()=>{
+// START THE SERVER
+app.listen(port, () => {
     console.log(`The application started successfully on port ${port}`);
 });
